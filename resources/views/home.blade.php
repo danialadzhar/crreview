f68f6f0f9323809b71409ca029d784ba61ce3d97
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-01 15:19:01 +0800

    Add center

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="row px-5">
     <div class="col-md-6">
-        <div class="card">
-            <div class="card-body py-4 px-4 mx-auto">
-                <i class="far fa-file-alt text-primary fa-7x"></i>
+        <div class="card shadow">
+            <div class="card-body py-5 px-5 text-center">
+                <i class="far fa-file-alt text-primary fa-7x d-flex justify-content-center"></i>
                 <h3 class="mt-3">CV Review</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -14,9 +14,9 @@
         </div>
     </div>
     <div class="col-md-6">
-        <div class="card">
-            <div class="card-body py-4 px-4 mx-auto">
-                <i class="fas fa-hands-helping text-primary fa-5x"></i>
+        <div class="card shadow">
+            <div class="card-body py-5 px-5 text-center">
+                <i class="fas fa-hands-helping text-primary fa-5x d-flex justify-content-center"></i>
                 <h3 class="mt-3">Apply Consultant</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
