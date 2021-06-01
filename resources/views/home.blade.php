168845bf593d4c325a454cd51d8bcc1cb1b732d6
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-01 15:09:27 +0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,5 +13,16 @@
             </div>
         </div>
     </div>
+    <div class="col-md-6">
+        <div class="card">
+            <div class="card-body py-4 px-4">
+                <i class="far fa-file-alt text-primary text-center"></i>
+                <h3>CV Review</h3>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
