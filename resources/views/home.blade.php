f9bd94482bb837f4e03bc2abde583961f189f02e
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-01 15:19:39 +0800

    Update icon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-6">
         <div class="card shadow">
             <div class="card-body py-5 px-5 text-center">
-                <i class="far fa-file-alt text-primary fa-7x d-flex justify-content-center"></i>
+                <i class="far fa-file-alt text-primary fa-5x d-flex justify-content-center"></i>
                 <h3 class="mt-3">CV Review</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
