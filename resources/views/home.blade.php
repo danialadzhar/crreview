76bc3f77f1d75099147e0cecafc927d459c774e0
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-01 15:15:56 +0800

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,9 @@
 <div class="row px-5">
     <div class="col-md-6">
         <div class="card">
-            <div class="card-body py-4 px-4 justify-content-center">
+            <div class="card-body py-4 px-4 mx-auto">
                 <i class="far fa-file-alt text-primary fa-7x"></i>
-                <h3>CV Review</h3>
+                <h3 class="mt-3">CV Review</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
@@ -15,9 +15,9 @@
     </div>
     <div class="col-md-6">
         <div class="card">
-            <div class="card-body py-4 px-4 justify-content-center">
-                <i class="far fa-file-alt text-primary fa-7x"></i>
-                <h3>CV Review</h3>
+            <div class="card-body py-4 px-4 mx-auto">
+                <i class="fas fa-hands-helping text-primary fa-5x"></i>
+                <h3 class="mt-3">Apply Consultant</h3>
                 <p>
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
