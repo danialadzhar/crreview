c569b0bf455c57d0a2c052659a00c85cee8fe95d
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-02 08:25:05 +0800

    Update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,13 +64,32 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
-            'user_id' => uniqid(),
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'status' => 'unpaid',
-            'role' => 'customer'
-        ]);
+        $users = User::all();
+        $get_pre_user_id = User::orderBy('id','Desc')->first();
+
+        if($users->isEmpty())
+        {
+            return User::create([
+                'user_id' => '001',
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'status' => 'unpaid',
+                'role' => 'customer'
+            ]);
+
+        }else{
+
+            $user_id = '00' . $get_pre_user_id->id + 1;
+            return User::create([
+                'user_id' => $user_id,
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+                'status' => 'unpaid',
+                'role' => 'customer'
+            ]);
+        }
+
     }
 }
