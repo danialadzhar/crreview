fa5cdcca70e85468eafef6a88c961c07fc464822
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-04 22:15:31 +0800

    Add alert

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -29,12 +29,16 @@
                                     <h4>Please Sign In</h4>
                                     <hr>
                                 </div>
-                                <div class="alert alert-warning alert-dismissible fade show" role="alert">
-                                    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
+                                @if(session('error'))
+                                    <div class="col-md-12">
+                                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                            {{ session('error') }}
+                                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                @endif
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <input type="email" name="email" class="form-control" placeholder="Email Address" required autofocus>
