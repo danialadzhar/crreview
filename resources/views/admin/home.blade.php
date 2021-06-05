e09082ba21ffbe079aebb4d22fd87e3fa2e4e284
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-05 14:39:31 +0800

    Update

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,8 +6,52 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-12">
-        <h2>Hi, Danial</h2>
+    <div class="col-md-8">
+        <h3>List Consultant</h3>
+        <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="col-md-4">
+        <div class="card shadow">
+            <div class="card-body py-4 px-4">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>78</h1>
+                    </div>
+                    <div class="col-md-12">
+                        <p><i class="fas fa-exclamation-circle text-secondary"></i> Total Consultant</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
