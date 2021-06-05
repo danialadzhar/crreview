5e125a72e067cf5ad68c1ed64921f14f91549971
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-05 14:53:00 +0800

    Update

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,26 +7,30 @@
 @section('content')
 <div class="row mt-5">
     <div class="col-md-8">
-        <h3>Pending Consultant</h3>
-        <hr>
-        <table class="table">
-            <thead class="thead-dark">
-              <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                  <td>1</td>
-                  <td>Danial Adzhar</td>
-                  <td>danial0597@gmail.com</td>
-                  <td><a class="btn btn-light btn-sm"><i class="fas fa-eye"></i></a></td>
-              </tr>
-            </tbody>
-        </table>
+        <div class="card shadow">
+            <div class="card-body py-4 px-4">
+                <h3>Pending Consultant</h3>
+                <hr>
+                <table class="table">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Danial Adzhar</td>
+                        <td>danial0597@gmail.com</td>
+                        <td><a class="btn btn-light btn-sm"><i class="fas fa-eye"></i></a></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
     <div class="col-md-4">
         <div class="card shadow">
