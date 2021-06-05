a1cda377079ba0567d42b45ca94b34e8a9e48229
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-05 22:31:20 +0800

    Update

--- resources/views/admin/pages/consultant/index.blade.php
+++ resources/views/admin/pages/consultant/index.blade.php
@@ -1,13 +1,13 @@
 @extends('admin.layouts.app')
 
 @section('title')
-    Consultant
+    @if($user_status == 'pending') Pending @endif @if($user_status == 'approved') Approved @endif Consultant
 @endsection
 
 @section('content')
 <div class="row mt-5">
     <div class="col-md-12">
-        <h2>List Consultant</h2>
+        <h2>@if($user_status == 'pending') Pending @endif @if($user_status == 'approved') Approved @endif Consultant</h2>
         <hr>
     </div>
     <div class="col-md-12">
