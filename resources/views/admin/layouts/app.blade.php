a059383258437a9c7830e3b7a62e73970fc3c690
Mohd Danial Bin Mohd Adzhar <10921402+danialadzhar@users.noreply.github.com>
2021-06-07 21:58:25 +0800

    Update

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand text-primary" href="{{ url('admin') }}">Get Me Hire | Admin</a>
+        <a class="navbar-brand text-primary" href="{{ url('admin') }}"><span class="badge badge-primary">Get Me</span> Hire</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
